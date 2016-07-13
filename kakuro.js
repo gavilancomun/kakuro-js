43deb010d1958b78324c9b1c23f0235ac4711709
gavilancomun <gavilancomun@gmail.com>
2016-07-13 16:17:42 +0100

    part

--- kakuro.js
+++ kakuro.js
@@ -101,6 +101,31 @@ var take = function(n, coll) {
   return result;
 };
 
+var partitionBy = function(f, coll) {
+  if (0 === coll.length) {
+    return [];
+  }
+  else {
+    var head = coll[0];
+    var fx = f(head);
+    var group = takeWhile(y => fx === f(y), coll);
+    return concatLists([group], partitionBy(f, drop(group.length, coll)));
+  }
+};
+
+var partitionAll = function(n, step, coll) {
+  if (0 === coll.length) {
+    return [];
+  }
+  else {
+    return concatLists([take(n, coll)], partitionAll(n, step, drop(step, coll)));
+  }
+};
+
+var partitionN = function(n, coll) {
+  return partitionAll(n, n, coll);
+};
+
 var assertEquals = function(expected, result) {
   if (expected !== result) {
     console.log("ERROR: expected " + expected + " got " + result);
@@ -153,10 +178,34 @@ function testTake() {
   assertEquals(4, result.length);
 }
 
+function testPartBy() {
+  var data = [1, 2, 2, 2, 3, 4, 5, 5, 6, 7, 7, 8, 9];
+  var result = partitionBy(n => 0 === (n % 2), data);
+  console.log(result);
+  assertEquals(9, result.length);
+}
+
+function testPartAll() {
+  var data = [1, 2, 2, 2, 3, 4, 5, 5, 6, 7, 7, 8, 9];
+  var result = partitionAll(5, 3, data);
+  console.log(result);
+  assertEquals(5, result.length);
+}
+
+function testPartN() {
+  var data = [1, 2, 2, 2, 3, 4, 5, 5, 6, 7, 7, 8, 9];
+  var result = partitionN(5, data);
+  console.log(result);
+  assertEquals(3, result.length);
+}
+
 testPermute();
 testTranspose();
 testTakeWhile();
 testConcat();
 testDrop();
 testTake();
+testPartBy();
+testPartAll();
+testPartN();
 
