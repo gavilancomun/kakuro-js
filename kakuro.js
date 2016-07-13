10df985691551ed807116165a813fccc948c53d4
gavilancomun <gavilancomun@gmail.com>
2016-07-13 16:07:24 +0100

    take

--- kakuro.js
+++ kakuro.js
@@ -21,6 +21,10 @@ var conj = function(arr, value) {
   return arr.concat([value]);
 };
 
+var concatLists = function(coll1, coll2) {
+  return coll1.concat(coll2);
+};
+
 var allDifferent = function(arr) {
   return arr.length === new Set(arr).size;
 };
@@ -44,13 +48,64 @@ var permuteAll = function(vs, target) {
   return permute(vs, target, []);
 };
 
+var transpose = function(m) {
+  if (0 === m.length) {
+    return [];
+  }
+  else {
+    var result = [];
+    for (var i = 0; i < m[0].length; ++i) {
+      let row = [];
+      for (var col of m) {
+        row.push(col[i]);
+      }
+      result.push(row);
+    }
+    return result;
+  }
+};
+
+var takeWhile = function(f, coll) {
+  var result = [];
+  for (var item of coll) {
+    if (!f(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+var drop = function(n, coll) {
+  var result = [];
+  var count = 1;
+  for (var item of coll) {
+    if (count > n) {
+      result.push(item);
+    }
+    ++count;
+  }
+  return result;
+};
+
+var take = function(n, coll) {
+  var result = [];
+  var count = 1;
+  for (var item of coll) {
+    if (count > n) {
+      return result;
+    }
+    result.push(item);
+    ++count;
+  }
+  return result;
+};
+
 var assertEquals = function(expected, result) {
   if (expected !== result) {
     console.log("ERROR: expected " + expected + " got " + result);
   }
 };
-
-console.log(v());
 
 function testPermute() {
   var vs = [v(), v(), v()];
@@ -61,5 +116,47 @@ function testPermute() {
   assertEquals(6, diff.length);
 }
 
+function testTranspose() {
+  var ints = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]];
+  var tr = transpose(ints);
+  console.log(ints);
+  console.log(tr);
+  assertEquals(ints.length, tr[0].length);
+  assertEquals(ints[0].length, tr.length);
+}
+
+function testTakeWhile() {
+  var result = takeWhile(n => n < 4, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  console.log(result);
+  assertEquals(4, result.length);
+}
+
+function testConcat() {
+  var a = [1, 2, 3];
+  var b = [4, 5, 6, 1, 2, 3];
+  var result = concatLists(a, b);
+  console.log(result);
+  assertEquals(9, result.length);
+}
+
+function testDrop() {
+  var a = [1, 2, 3, 4, 5, 6];
+  var result = drop(4, a);
+  console.log(result);
+  assertEquals(2, result.length);
+}
+
+function testTake() {
+  var a = [1, 2, 3, 4, 5, 6];
+  var result = take(4, a);
+  console.log(result);
+  assertEquals(4, result.length);
+}
+
 testPermute();
+testTranspose();
+testTakeWhile();
+testConcat();
+testDrop();
+testTake();
 
